048923667d1db1e3b906efcb22b331580037e546
mira_arar <mira.ar230@gmail.com>
2020-07-18 16:19:57 +0300

    user authentication

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginForm from '../auth/login/Login';
+import SignUpForm from '../auth/signUp/SignUp';
+
+export default function Home() {
+    return (
+        <div>
+           <LoginForm/> 
+            <SignUpForm/>
+        </div>
+    )
+}
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../App.scss';
-
+import Home from '../components/home/Home';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Hello</h1>
+			<Home/>
 		</div>
 	);
 }
