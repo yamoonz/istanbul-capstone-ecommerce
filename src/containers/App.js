7c475699ed277dca00a163c16c651331d31b9f83
mira_arar <mira.ar230@gmail.com>
2020-07-18 10:17:41 +0300

    eslint added

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,11 +3,11 @@ import '../App.scss';
 
 
 function App() {
-  return (
-    <div className="App">
-     <h1>Hello</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Hello</h1>
+		</div>
+	);
 }
 
 export default App;
