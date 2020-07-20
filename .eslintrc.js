e8d37afae8bac326567b4f709f39161b1335ca83
mira_arar <mira.ar230@gmail.com>
2020-07-20 11:43:20 +0300

    linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-  },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 11,
-    sourceType: "module",
-  },
-  plugins: ["react"],
-  rules: {
-    indent: ["error", "tab"],
-    "linebreak-style": ["error", "windows"],
-    quotes: ["error", "single"],
-    semi: ["error", "always"],
-  },
+	env: {
+		browser: true,
+		es2020: true,
+	},
+	extends: ['eslint:recommended', 'plugin:react/recommended'],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 11,
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'windows'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+	},
 };
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,14 @@
-import React from "react";
-import "../App.scss";
-import Home from "../components/home/Home";
+import React from 'react';
+import '../App.scss';
+import Home from '../components/home/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-      <Home />
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Hello</h1>
+			<Home />
+		</div>
+	);
 }
 
 export default App;
