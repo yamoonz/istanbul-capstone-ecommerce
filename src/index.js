8595e4a4d9518ea8f5bb3d0772709035cfd8bdf5
Omar Alithawi <oadnan100@gmail.com>
2020-07-21 12:12:02 +0300

    creating add products form branch

--- src/index.js
+++ src/index.js
@@ -27,6 +27,6 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// unregister() to register() below. Note this comes with some pitfalls.
+// unregister() to register() below. Note this comes with some pitfalls..
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
