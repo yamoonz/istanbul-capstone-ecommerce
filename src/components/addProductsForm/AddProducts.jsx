874dc207b718e7c20e8272236dacf34ca58832fb
mira_arar <mira.ar230@gmail.com>
2020-07-21 11:43:27 +0300

    login with google and facebook

--- src/components/addProductsForm/AddProducts.jsx
+++ src/components/addProductsForm/AddProducts.jsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+import React from "react";
+import { Form, Button } from "react-bootstrap";
 
 export default function AddProducts() {
-	return (
-		<>
-			<Form>
-				<Form.Group>
-					<Form.Control type="text" placeholder="brand" />
-				</Form.Group>
-				<Form.Group>
-					<Form.Control type="text" placeholder="brand" />
-				</Form.Group>
-				<Form.Group>
-					<Form.Control type="text" placeholder="brand" />
-				</Form.Group>
-				<Form.Group>
-					<Form.Control type="text" placeholder="brand" />
-				</Form.Group>
-				<Form.Group>
-					<Form.Control type="text" placeholder="brand" />
-				</Form.Group>
+  return (
+    <>
+      <Form>
+        <Form.Group>
+          <Form.Control type="text" placeholder="brand" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Control type="text" placeholder="brand" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Control type="text" placeholder="brand" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Control type="text" placeholder="brand" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Control type="text" placeholder="brand" />
+        </Form.Group>
 
-				<Button variant="primary" type="submit">
+        <Button variant="primary" type="submit">
           Submit
-				</Button>
-			</Form>
-		</>
-	);
+        </Button>
+      </Form>
+    </>
+  );
 }
