bb62f9db9684b76714b775d34d4a8383df8aa997
mira_arar <mira.ar230@gmail.com>
2020-07-23 16:52:14 +0300

    circleci cnfiguration

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
-import './App.scss';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-
 import { Provider } from 'react-redux';
 import allReducers from './components/redux/reducers';
 
@@ -28,6 +26,6 @@ ReactDOM.render(
 );
 
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
+// unregister() to register() below. Note this comes with some pitfalls..
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
