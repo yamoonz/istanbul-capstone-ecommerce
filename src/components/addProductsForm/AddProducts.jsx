ba6908129ffa4bd32804b2a4dea5ea710ccb1bf7
Mira Arar <62375758+MiraAr@users.noreply.github.com>
2020-07-29 23:23:22 +0300

    Authentication (#26)

--- src/components/addProductsForm/AddProducts.jsx
+++ src/components/addProductsForm/AddProducts.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function AddProducts() {
-  return <div></div>;
+  return <div>add Products</div>;
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import LoginForm from "../auth/login/Login";
+import SignUpForm from "../auth/signUp/SignUp";
 import LanguageDropdown from "./LanguageDropdown";
 
 export default function Home() {
-  return <LanguageDropdown />;
+  return (
+    <div>
+      <LoginForm />
+      <SignUpForm />
+      <LanguageDropdown />
+    </div>
+  );
 }
