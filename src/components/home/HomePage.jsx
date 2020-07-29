af45b63c507469ca58eae267483bda855e0342fa
emrerdem1 <emrerdem94@gmail.com>
2020-07-29 21:37:06 +0300

    solve conflicts

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../layout/nav/Navbar";
+import LanguageDropdown from "./LanguageDropdown";
 
 const HomePage = () => {
   return (
     <>
       <Navbar />
+      <LanguageDropdown />
     </>
   );
 };
