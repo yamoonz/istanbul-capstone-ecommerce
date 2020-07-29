fbecf751de148049cecf61b8e9303ac679ba074d
emrerdem1 <emrerdem94@gmail.com>
2020-07-29 22:27:47 +0300

    exclude navbar styling from branch

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "../App.scss";
-import HomePage from "../components/home/HomePage";
 import { RouteBlock } from "./RouteBlock";
 
 function App() {
   return (
     <>
-      <HomePage />
       <RouteBlock />
     </>
   );
