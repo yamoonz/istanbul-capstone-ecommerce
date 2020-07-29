04d21b629d95e072cb36ba3489691368ad5a4887
Omar Alithawi <oadnan100@gmail.com>
2020-07-29 14:11:31 +0300

    refactoring product card component

--- src/components/layout/productCard/ProductCard.jsx
+++ src/components/layout/productCard/ProductCard.jsx
@@ -6,7 +6,7 @@ import "./style.scss";
 import heartBroken from "../../../assets/svgs/heart-broken.svg";
 
 export default function ProductCard(props) {
-  // we will load isLiked from firebase and then set it to this state
+  // We will load isLiked from firebase and then set it to this state
   const [isLiked, setIsLiked] = React.useState(false);
 
   const settings = {
@@ -21,36 +21,42 @@ export default function ProductCard(props) {
     autoplaySpeed: 5000,
   };
 
+  const likeBtn = isLiked ? (
+    <img
+      onClick={() => setIsLiked(!isLiked)}
+      src={heart}
+      className="likeIcon"
+      alt="heart"
+    />
+  ) : (
+    <img
+      onClick={() => setIsLiked(!isLiked)}
+      src={heartBroken}
+      className="likeIcon"
+      alt="heart broken"
+    />
+  );
+
+  const slider = (
+    <Slider {...settings}>
+      <div>
+        <img src={props.info.images[0]} alt="image" />
+      </div>
+      <div>
+        <img src={props.info.images[1]} alt="image" />
+      </div>
+      <div>
+        <img src={props.info.images[2]} alt="image" />
+      </div>
+    </Slider>
+  );
+
   return (
     <React.Fragment>
       <Col xs={8} md={5} lg={3} className="productCard">
-        {/*when we get the real product we need to add an id to each product*/}
-        {isLiked ? (
-          <img
-            onClick={() => setIsLiked(!isLiked)}
-            src={heart}
-            className="likeIcon"
-            alt="heart"
-          />
-        ) : (
-          <img
-            onClick={() => setIsLiked(!isLiked)}
-            src={heartBroken}
-            className="likeIcon"
-            alt="heart broken"
-          />
-        )}
-        <Slider {...settings}>
-          <div>
-            <img src={props.info.images[0]} alt="image" />
-          </div>
-          <div>
-            <img src={props.info.images[1]} alt="image" />
-          </div>
-          <div>
-            <img src={props.info.images[2]} alt="image" />
-          </div>
-        </Slider>
+        {/*When we get the real product we need to add an id to each product*/}
+        {likeBtn}
+        {slider}
         <div className="title">{props.info.title}</div>
         <div className="price">${props.info.price}</div>
         <div className="brand">{props.info.brand}</div>
