ae1f5cb3416309b401062758a300adf6884b0a38
Yamoonz <yamooonz@gmail.com>
2020-07-30 17:17:23 +0300

    edited requested

--- src/components/home/Footer.jsx
+++ src/components/home/Footer.jsx
@@ -1,41 +1,40 @@
-import React from "react";
+import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faFacebook,
-  faTwitter,
-  faGoogle,
-  faGithub,
+    faFacebook,
+    faTwitter,
+    faGoogle,
+    faGithub
 } from "@fortawesome/free-brands-svg-icons";
-import "./style.scss";
+import './style.scss';
 
 const Footer = () => {
-  return (
-    <div>
-      <hr />
-      <footer>
-        <div className="icons">
-          <a href="https://www.twitter.com/" className="twitter social">
-            <FontAwesomeIcon icon={faTwitter} size="2x" />
-          </a>
-          <a href="https://www.gmail.com/" className="Gmail social">
-            <FontAwesomeIcon icon={faGoogle} size="2x" />{" "}
-          </a>
-          <a href="https://www.facebook.com/" className="facebook social">
-            <FontAwesomeIcon icon={faFacebook} size="2x" />
-          </a>
-          <a href="https://www.github.com/" className="github social">
-            <FontAwesomeIcon icon={faGithub} size="2x" />
-          </a>
-        </div>
-        <ul className="navBar">
-          <li> Home</li>
-          <li>Products</li>
-          <li>Blog</li>
-          <li>Conatct</li>
-        </ul>
-      </footer>
+    return <div>
+        <hr />
+        <footer>
+            <div className='icons'>
+                <a href="https://www.twitter.com/" className="twitter social">
+                    <FontAwesomeIcon icon={faTwitter} size="2x" />
+                </a>
+                <a href="https://www.gmail.com/" className="Gmail social">
+                    <FontAwesomeIcon icon={faGoogle} size="2x" /> </a>
+                <a href="https://www.facebook.com/" className="facebook social">
+                    <FontAwesomeIcon icon={faFacebook} size="2x" /></a>
+                <a href="https://www.github.com/" className="github social">
+                    <FontAwesomeIcon icon={faGithub} size="2x" /></a>
+            </div>
+            <ul className='footerNav'>
+                <li>  Home </li>
+
+                <li> Products </li>
+
+                <li> Blog </li>
+
+                <li> Conatct </li>
+            </ul>
+            <span dangerouslySetInnerHTML={{ "__html": "&copy; 2020" }} />
+        </footer>
     </div>
-  );
 };
 
 export default Footer;
