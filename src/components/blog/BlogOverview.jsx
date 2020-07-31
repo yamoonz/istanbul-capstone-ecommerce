e9456367b54f4ccd37b4b8f1cbfef10ac31ac2ee
mira_arar <mira.ar230@gmail.com>
2020-07-31 13:32:53 +0300

    adding single blog page

--- src/components/blog/BlogOverview.jsx
+++ src/components/blog/BlogOverview.jsx
@@ -1,5 +1,47 @@
 import React from "react";
+import SingleBlogPage from "./SingleBlogPage";
 
 export default function BlogOverview() {
-  return <div></div>;
+  const healthBLogUrl = "http://127.0.0.1/wordpress/wp-json/wp/v2/health/";
+  const imgUrl = "http://127.0.0.1/wordpress/wp-json/wp/v2/media/";
+  const [wpData, setWpData] = React.useState([]);
+  const [wpImages, setWpImages] = React.useState([]);
+
+  const fetchFromWP = async () => {
+    let response1 = await fetch(healthBLogUrl);
+    let blogContentdData = await response1.json();
+    setWpData(blogContentdData);
+  };
+  const fetchImgsFromWP = async () => {
+    let response2 = await fetch(imgUrl);
+    let blogImagesData = await response2.json();
+    setWpImages(blogImagesData);
+  };
+
+  const fetchData = async () => {
+    await fetchImgsFromWP();
+    await fetchFromWP();
+  };
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <div className="App">
+        {wpData.map((article, index) => {
+          return (
+            // <Route path={`${path}/:topicId`}>
+            <SingleBlogPage
+              content={article}
+              image={wpImages[index]}
+              key={index}
+            />
+            // </Route>
+          );
+        })}
+      </div>
+    </>
+  );
 }
--- src/components/blog/SingleBlogPage.jsx
+++ src/components/blog/SingleBlogPage.jsx
@@ -1,5 +1,27 @@
 import React from "react";
+import "./singleBlogpage.scss";
 
-export default function SingleBlogPage() {
-  return <div></div>;
+export default function SingleBlogPage(props) {
+  let title = props.content.title.rendered;
+  let date = props.content.date.split("T")[0];
+  let type = props.content.type;
+  let content = props.content.content.rendered;
+  console.log(props);
+
+  return (
+    <div className="blogContainer">
+      <h6 dangerouslySetInnerHTML={{ __html: type }} className="blogType" />
+      <div className="blogHeader">
+        <h1 dangerouslySetInnerHTML={{ __html: title }} className="blogTitle" />
+        <h6 dangerouslySetInnerHTML={{ __html: date }} className="blogDate" />
+      </div>
+      <div>
+        <img src={props.image.source_url} className="mainImg" />
+      </div>
+      <div
+        dangerouslySetInnerHTML={{ __html: content }}
+        className="blogContent"
+      />
+    </div>
+  );
 }
