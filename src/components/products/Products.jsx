76e9cd73dd2b05ffb84eb951c7cf80617d245049
emrerdem1 <emrerdem94@gmail.com>
2020-08-01 22:52:07 +0300

    move nested route to above of its parent

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import ProductCard from "../layout/productCard/ProductCard";
 import { Container, Row } from "react-bootstrap";
 import { MOCK_DATABASE } from "../common/MockDatabase";
-import { Route, NavLink } from "react-router-dom";
-import SingleProductPage from "./product/SingleProductPage";
+import { NavLink } from "react-router-dom";
 
 export default function Products() {
   // We don't want to use a dynamic path finder like useRouteMatch because we are having this component in HomePage as well.
@@ -13,13 +12,10 @@ export default function Products() {
     <Container>
       <Row>
         {MOCK_DATABASE.map((info, index) => (
-          <React.Fragment key={index}>
-            <NavLink to={`/products/${info.id}`}>
-              <ProductCard info={info} index={index} />
-            </NavLink>
-          </React.Fragment>
+          <NavLink to={`/products/${info.id}`} key={index}>
+            <ProductCard info={info} index={index} />
+          </NavLink>
         ))}
-        <Route path={`/products/:productId`} component={SingleProductPage} />
       </Row>
     </Container>
   );
--- src/containers/RouteBlock.jsx
+++ src/containers/RouteBlock.jsx
@@ -7,12 +7,14 @@ import AboutPage from "../components/about/About";
 import ProductsPage from "../components/products/Products";
 import AdminPage from "../components/addProductsForm/AddProducts";
 import Home from "../components/home/Home";
+import SingleProductPage from "../components/products/product/SingleProductPage";
 
 export const RouteBlock = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/signup" component={SignUp} />
+      <Route path="/products/:productId" component={SingleProductPage} />
       <Route path="/products" component={ProductsPage} />
       <Route path="/shoppingcart" component={CartPage} />
       <Route path="/blog" component={BlogOverview} />
