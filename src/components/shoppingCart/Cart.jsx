9735a8b6026c43bb0692e45644a6f9046e068efe
Yamoonz <yamooonz@gmail.com>
2020-08-03 17:20:25 +0300

    updtaed

--- src/components/shoppingCart/Cart.jsx
+++ src/components/shoppingCart/Cart.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { Component } from 'react';
 
-const Cart = () => {
-  return <div></div>;
-};
+class Cart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicks: 0,
+      show: true
+    };
+  }
+
+  IncrementItem = () => {
+    this.setState({ clicks: this.state.clicks + 1 });
+  }
+  DecreaseItem = () => {
+    this.setState({ clicks: this.state.clicks - 1 });
+  }
+  ToggleClick = () => {
+    this.setState({ show: !this.state.show });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.IncrementItem}>+ </button>
+        <button onClick={this.DecreaseItem}>-</button>
+        <button onClick={this.ToggleClick}>
+          {this.state.show ? 'Hide number' : 'Show number'}
+        </button>
+        {this.state.show ? <h2>{this.state.clicks}</h2> : ''}
+      </div>
+    );
+  }
+}
 
 export default Cart;
