d596ebdd86e55742ef1a84cd7c69e9266651e380
emrerdem1 <emrerdem94@gmail.com>
2020-08-06 20:05:55 +0300

    Fix: Netlify warnings of reduntant alt tags of imgs etc

--- src/components/auth/signUp/SignUp.jsx
+++ src/components/auth/signUp/SignUp.jsx
@@ -5,25 +5,24 @@ import db from "../../config/firebaseConfig";
 export default function SignUp() {
   const createNewUser = async (e) => {
     e.preventDefault();
-    const userName = e.target[0].value;
+
     const email = e.target[1].value;
     const password = e.target[2].value;
+    const userName = e.target[0].value;
     const newUser = await auth.createUserWithEmailAndPassword(email, password);
-    const createNewSubCollection = await db
-      .collection("users")
-      .doc(newUser.user.uid)
-      .collection("liked")
-      .add({
-        liked: "0",
-      });
-    const setNewUserData = await db
-      .collection("users")
-      .doc(newUser.user.uid)
-      .set({
-        name: userName,
-        isAdmin: false,
-      });
+
+    // TODO(MiraAr): To use this function that creates new subcollection
+    await db.collection("users").doc(newUser.user.uid).collection("liked").add({
+      liked: "0",
+    });
+
+    // TODO(MiraAr): To use this function that set new user data
+    await db.collection("users").doc(newUser.user.uid).set({
+      name: userName,
+      isAdmin: false,
+    });
   };
+
   return (
     <div>
       <h5>Sign Up</h5>
