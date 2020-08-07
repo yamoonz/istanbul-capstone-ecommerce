c204eb1cd788040a695f03d131ed37afc1206f92
emrerdem1 <emrerdem94@gmail.com>
2020-08-08 01:16:14 +0300

    Import: add slidersection

--- src/components/shoppingCart/Cart.jsx
+++ src/components/shoppingCart/Cart.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import PaymentSum from "./paymentbar/PaymentSum";
+import RelatedProducts from "./relatedproducts/RelatedProducts";
 
 const Cart = () => {
   return (
     <>
       <PaymentSum />
+      <RelatedProducts />
     </>
   );
 };
---
+++ src/components/shoppingCart/relatedproducts/RelatedProducts.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SliderSection from "../../singleProductPage/SliderSection";
+
+const RelatedProducts = () => {
+  return (
+    <>
+      <SliderSection />
+    </>
+  );
+};
+
+export default RelatedProducts;
