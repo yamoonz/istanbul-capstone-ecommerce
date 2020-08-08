7bcb9be13034f14db272a4532e2bedbc1e20b76a
Omar Alithawi <oadnan100@gmail.com>
2020-08-08 17:00:39 +0300

    about page

---
+++ src/components/about/OurTeam.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { teamData } from "../common/teamData";
+import { Container, Row, Col } from "react-bootstrap";
+
+export default function OurTeam() {
+  return (
+    <Container className="ourTeamContainer">
+      <h1 className="ourTeamTitle">Our Team</h1>
+      <Row className="ourTeamRow">
+        {teamData.map((member, index) => {
+          return (
+            <Col key={index} xs={8} md={5} lg={3} className="teamMember">
+              <img src={member.image} alt="member" />
+              <h4>{member.name}</h4>
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
+}
---
+++ src/components/common/teamData.js
@@ -0,0 +1,32 @@
+import mira from "../../assets/img/mira.jpg";
+import omar from "../../assets/img/omar.jpg";
+import emre from "../../assets/img/emre.jpg";
+import yaman from "../../assets/img/yaman.jpg";
+import burak from "../../assets/img/burak.png";
+
+export const teamData = [
+  {
+    name: "Omar Alithawi",
+    image: omar,
+  },
+
+  {
+    name: "Burak Selvi",
+    image: burak,
+  },
+
+  {
+    name: "Mira Arar",
+    image: mira,
+  },
+
+  {
+    name: "Emre Erdem",
+    image: emre,
+  },
+
+  {
+    name: "Yaman Kahveci",
+    image: yaman,
+  },
+];
