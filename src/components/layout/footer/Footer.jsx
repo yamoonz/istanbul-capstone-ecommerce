cf5e65efe16f250cb77fa8ff4e11802574069306
emrerdem1 <emrerdem94@gmail.com>
2020-08-09 15:38:20 +0300

    Create: new footer layout

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -6,41 +6,56 @@ import {
   faGoogle,
   faGithub,
 } from "@fortawesome/free-brands-svg-icons";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
 import { NavLink } from "react-router-dom";
-import "./style.scss";
+import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <footer className="footerSection">
-      <div className="footerIcons">
-        <a href="https://www.twitter.com/" className="twitter social">
-          <FontAwesomeIcon icon={faTwitter} size="2x" />
-        </a>
-        <a href="https://www.gmail.com/" className="gmail social">
-          <FontAwesomeIcon icon={faGoogle} size="2x" />
-        </a>
-        <a href="https://www.facebook.com/" className="facebook social">
-          <FontAwesomeIcon icon={faFacebook} size="2x" />
-        </a>
-        <a href="https://www.github.com/" className="github social">
-          <FontAwesomeIcon icon={faGithub} size="2x" />
-        </a>
-      </div>
-      <ul className="navBar">
-        <NavLink to="/">
-          <li className="navBarItem">Home</li>
-        </NavLink>
-        <NavLink to="/products">
-          <li className="navBarItem">Products</li>
-        </NavLink>
-        <NavLink to="/blog">
-          <li className="navBarItem">Blog</li>
-        </NavLink>
-        <NavLink to="/about">
-          <li className="navBarItem">About</li>
-        </NavLink>
-      </ul>
-    </footer>
+    <Container fluid className="footerWrapper">
+      <Row className="footerLinks">
+        <Col className="footerLinksLogo">
+          <h3 className="footerLogo">LOGO</h3>
+          <span className="footerMotto">Your health is in good hands.</span>
+        </Col>
+        <Col className="footerLinksBlogs">
+          <NavLink to="/blog">Blog</NavLink>
+          <NavLink to="/blog">Skin Care</NavLink>
+          <NavLink to="/blog">Stay Fit and Healthy</NavLink>
+        </Col>
+        <Col className="footerLinksPages">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/products">Products</NavLink>
+          <NavLink to="/about">About</NavLink>
+        </Col>
+      </Row>
+      <Row className="footerSeparator"></Row>
+      <Row className="footerIconsCopyright">
+        <Col className="footerIcons">
+          <a
+            href="https://www.twitter.com/"
+            className="footerSocialIcon twitter"
+          >
+            <FontAwesomeIcon icon={faTwitter} size="2x" />
+          </a>
+          <a href="https://www.gmail.com/" className="footerSocialIcon google">
+            <FontAwesomeIcon icon={faGoogle} size="2x" />
+          </a>
+          <a
+            href="https://www.facebook.com/"
+            className="footerSocialIcon facebook"
+          >
+            <FontAwesomeIcon icon={faFacebook} size="2x" />
+          </a>
+          <a href="https://www.github.com/" className="footerSocialIcon github">
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>
+        </Col>
+        <Col className="footerCopyright">&copy; All rights reserved.</Col>
+      </Row>
+    </Container>
   );
 };
 
