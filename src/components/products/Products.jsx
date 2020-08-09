7146fd0dc272dd05aa3e76a4c9e7fe6573a1ced7
Omar Alithawi <oadnan100@gmail.com>
2020-08-09 22:24:57 +0300

    styling homepage products section

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import ProductCard from "../layout/productCard/ProductCard";
 import { Container, Row } from "react-bootstrap";
 import { MOCK_DATABASE } from "../common/MockDatabase";
+import "./style.scss";
 
 export default function Products() {
   // We don't want to use a dynamic path finder like useRouteMatch because we are having this component in HomePage as well.
   // In that case, it would render the `path` and `url` as `/` which will not work in our favour.
 
   return (
-    <Container>
+    <Container className="productsSectionContainer">
+      <h1>Products</h1>
       <Row>
         {MOCK_DATABASE.map((info, index) => (
           <ProductCard info={info} index={index} />
