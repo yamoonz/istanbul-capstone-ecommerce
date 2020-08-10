67e04b1d9d956b3b66f3301fb552ac19526d21f4
emrerdem1 <emrerdem94@gmail.com>
2020-08-10 14:16:25 +0300

    Add: import sliderSection

--- src/components/shoppingCart/Cart.jsx
+++ src/components/shoppingCart/Cart.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import PaymentSum from "./paymentbar/PaymentSum";
+import RelatedProducts from "./relatedsection/RelatedProducts";
 
 const Cart = () => {
   return (
     <>
       <PaymentSum />
+      <RelatedProducts />
     </>
   );
 };
---
+++ src/components/shoppingCart/relatedsection/RelatedProducts.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SliderSection from "../../singleProductPage/SliderSection";
+import "./RelatedProducts.scss";
+
+const RelatedProducts = () => {
+  return (
+    <>
+      <SliderSection />
+    </>
+  );
+};
+
+export default RelatedProducts;
