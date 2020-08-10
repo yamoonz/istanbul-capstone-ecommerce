48e83e708af6bca37470cf311d03c9c2b8d2321a
emrerdem1 <emrerdem94@gmail.com>
2020-08-10 22:10:02 +0300

    Edit: set fontsizes for responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import clock from "../../assets/video-clock.mov";
-import { Button } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import { NavLink } from "react-router-dom";
 import "./Header.scss";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
 
 export default function Header() {
   const { t } = useTranslation();
@@ -41,11 +43,29 @@ export default function Header() {
           </div>
         </div>
         <div className="bottomButtons">
-          <ul>
-            <li>Fast shipping</li>
-            <li className="ml-5">Education</li>
-            <li className="ml-5">Healthy life</li>
-          </ul>
+          <Row className="shortInfoRow">
+            <Col className="shortInfoItem">
+              <i className="fas fa-shipping-fast shippingIcon shortInfoIcon"></i>
+              <h3 className="shortInfoTitle">Fast shipping</h3>
+              <span className="shortInfoText">
+                Have your orders shipped right away!
+              </span>
+            </Col>
+            <Col className="shortInfoItem">
+              <i className="fas fa-award educationIcon shortInfoIcon"></i>
+              <h3 className="shortInfoTitle">Education</h3>
+              <span className="shortInfoText">
+                Learn how to distinct quality products!
+              </span>
+            </Col>
+            <Col className="shortInfoItem">
+              <i className="fas fa-medkit healthIcon shortInfoIcon"></i>
+              <h3 className="shortInfoTitle">Healthy life</h3>
+              <span className="shortInfoText">
+                Choose only natural products!
+              </span>
+            </Col>
+          </Row>
         </div>
       </div>
     </div>
