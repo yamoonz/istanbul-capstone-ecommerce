e76f3f21d9d98ea33092d66c74a18d86087d3a8a
emrerdem1 <emrerdem94@gmail.com>
2020-08-11 22:41:52 +0300

    Set: set general layout

--- src/components/shoppingCart/Cart.jsx
+++ src/components/shoppingCart/Cart.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import PaymentSum from "./paymentbar/PaymentSum";
 import CartDetail from "./cartdetail/CartDetail";
+import "./cartdetail/CartDetail.scss";
 
 const Cart = () => {
   return (
-    <>
+    <div className="shoppingCartWrapper">
       <PaymentSum />
       <CartDetail />
-    </>
+    </div>
   );
 };
 
--- src/components/shoppingCart/cartdetail/CartDetail.jsx
+++ src/components/shoppingCart/cartdetail/CartDetail.jsx
@@ -3,25 +3,33 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
-import "./CartDetail.scss";
+import { MOCK_DATABASE } from "../../common/MockDatabase";
+
+const { title, images, brand, price } = MOCK_DATABASE[0];
 
 const CartDetail = () => {
   return (
     <Container className="cartDetailWrapper">
       <Row className="cartDetailHeader">
         <Col className="cartDetailTitle">Shopping Card (X Items)</Col>
-        <Col className="backToShopping">
-          <Button>Back to Shopping!</Button>
+        <Col className="backToShoppingWrapper">
+          <Button className="backToShopping">Back to Shopping!</Button>
         </Col>
       </Row>
       <Row className="cartDetailMain">
         <Col xl={6} lg={6} md={6} sm={6} xs={6} className="cartProductsCol">
           <Row className="productTitleRow">Products</Row>
           <Row className="productInfoRow">
-            <Col className="productImage">Image</Col>
+            <Col className="productImageCol">
+              <img className="productImg" src={images[0]} alt={brand + title} />
+            </Col>
             <Col className="productTexts">
-              <Row className="productBrand">Nike</Row>
-              <Row className="productName">Airmax Z101</Row>
+              <Row className="productBrand">
+                <span className="productBrandText">{brand}</span>
+              </Row>
+              <Row className="productName">
+                <span className="productNameText">{title}</span>
+              </Row>
               <Row className="productDeleteSave">
                 <Col className="productDelete">Delete</Col>
                 <Col className="productSave">Save</Col>
@@ -42,7 +50,7 @@ const CartDetail = () => {
         <Col xl={3} lg={3} md={3} sm={3} xs={3} className="cartPriceCol">
           <Row className="priceTitleRow">Price</Row>
           <Row className="priceInfoRow">
-            <Col className="priceNumber">$100</Col>
+            <Col className="priceNumber">${price}</Col>
           </Row>
         </Col>
       </Row>
