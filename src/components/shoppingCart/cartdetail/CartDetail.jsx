582d91da1ef17c29780d868e77543d7548b3d339
emrerdem1 <emrerdem94@gmail.com>
2020-08-12 12:39:10 +0300

    Change: change layout to fit mapping function

--- src/components/shoppingCart/cartdetail/CartDetail.jsx
+++ src/components/shoppingCart/cartdetail/CartDetail.jsx
@@ -8,52 +8,112 @@ import { MOCK_DATABASE } from "../../common/MockDatabase";
 const { title, images, brand, price } = MOCK_DATABASE[0];
 
 const CartDetail = () => {
+  const cartDetailHeaderRow = (
+    <Row className="cartDetailHeader">
+      <Col className="cartDetailTitle">Shopping Card (X Items)</Col>
+      <Col className="backToShoppingWrapper">
+        <Button className="backToShopping">Back to Shopping!</Button>
+      </Col>
+    </Row>
+  );
+
+  const titleHeaderRow = (
+    <Row className="titleHeader">
+      <Col
+        xl={6}
+        lg={6}
+        md={6}
+        sm={6}
+        xs={6}
+        className="productsHeader titleHeaderItem"
+      >
+        Products
+      </Col>
+      <Col
+        xl={3}
+        lg={3}
+        md={3}
+        sm={3}
+        xs={3}
+        className="quantityHeader titleHeaderItem"
+      >
+        Quantity
+      </Col>
+      <Col
+        xl={3}
+        lg={3}
+        md={3}
+        sm={3}
+        xs={3}
+        className="priceHeader titleHeaderItem"
+      >
+        Price
+      </Col>
+    </Row>
+  );
+
+  const productDetailCols = (
+    <>
+      <Col
+        xl={6}
+        lg={6}
+        md={6}
+        sm={6}
+        xs={6}
+        className="productInfoCol productColItem"
+      >
+        <Col className="productImageCol">
+          <img className="productImg" src={images[0]} alt={brand + title} />
+        </Col>
+        <Col className="productTextCol">
+          <Row className="productBrand">
+            <span className="productTextSpan">{brand}</span>
+          </Row>
+          <Row className="productName">
+            <span className="productTextSpan productTextTitle">{title}</span>
+          </Row>
+          <Row className="productDeleteSave">
+            <Col className="productDelete">
+              <i className="fas fa-trash trashIcon"></i>
+              <span>Delete</span>
+            </Col>
+            <Col className="productSave">
+              <i className="far fa-bookmark bookmarkIcon"></i>
+              <span>Save</span>
+            </Col>
+          </Row>
+        </Col>
+      </Col>
+      <Col
+        xl={3}
+        lg={3}
+        md={3}
+        sm={3}
+        xs={3}
+        className="productQuantityCol productColItem"
+      >
+        <Button className="countModifier">-</Button>
+        <span className="countNumber">1</span>
+        <Button className="countModifier">+</Button>
+      </Col>
+      <Col
+        xl={3}
+        lg={3}
+        md={3}
+        sm={3}
+        xs={3}
+        className="productPriceCol productColItem"
+      >
+        ${price}
+      </Col>
+    </>
+  );
+
   return (
     <Container className="cartDetailWrapper">
-      <Row className="cartDetailHeader">
-        <Col className="cartDetailTitle">Shopping Card (X Items)</Col>
-        <Col className="backToShoppingWrapper">
-          <Button className="backToShopping">Back to Shopping!</Button>
-        </Col>
-      </Row>
-      <Row className="cartDetailMain">
-        <Col xl={6} lg={6} md={6} sm={6} xs={6} className="cartProductsCol">
-          <Row className="productTitleRow">Products</Row>
-          <Row className="productInfoRow">
-            <Col className="productImageCol">
-              <img className="productImg" src={images[0]} alt={brand + title} />
-            </Col>
-            <Col className="productTexts">
-              <Row className="productBrand">
-                <span className="productBrandText">{brand}</span>
-              </Row>
-              <Row className="productName">
-                <span className="productNameText">{title}</span>
-              </Row>
-              <Row className="productDeleteSave">
-                <Col className="productDelete">Delete</Col>
-                <Col className="productSave">Save</Col>
-              </Row>
-            </Col>
-          </Row>
-        </Col>
-        <Col xl={3} lg={3} md={3} sm={3} xs={3} className="cartQuantityCol">
-          <Row className="quantityTitleRow">Quantity</Row>
-          <Row className="quantityInfoRow">
-            <Col className="quantityInfoCol">
-              <Button className="countModifier">-</Button>
-              <span className="countNumber">1</span>
-              <Button className="countModifier">+</Button>
-            </Col>
-          </Row>
-        </Col>
-        <Col xl={3} lg={3} md={3} sm={3} xs={3} className="cartPriceCol">
-          <Row className="priceTitleRow">Price</Row>
-          <Row className="priceInfoRow">
-            <Col className="priceNumber">${price}</Col>
-          </Row>
-        </Col>
-      </Row>
+      {cartDetailHeaderRow}
+      {titleHeaderRow}
+      <Row className="cartDetailMain">{productDetailCols}</Row>
     </Container>
   );
 };
