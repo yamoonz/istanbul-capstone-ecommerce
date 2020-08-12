6b9b2749958e104a84a7e073446983b53bff1d0e
yamoonz <yamooonz@gmail.com>
2020-08-12 16:28:16 +0300

    fixed the path

--- src/components/profile/ProfilePage.jsx
+++ src/components/profile/ProfilePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import GeneralSection from "./general/GeneralSection";
-import MainSection from "../profilePage/ProfilePage";
+import UserFavoriteList from "./userFavoriteList/UserFavoriteList";
 const ProfilePage = () => {
   return (
     <>
