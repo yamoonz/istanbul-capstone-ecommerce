d9f22ad6274fcc5401ff24c8ceb512946b405fd4
yamoonz <yamooonz@gmail.com>
2020-08-13 22:00:56 +0300

    removed node modules from import

--- src/components/profile/UserFavoriteList.jsx
+++ src/components/profile/UserFavoriteList.jsx
@@ -1,4 +1,4 @@
-import React from "./node_modules/react";
+import React from "react";
 import { MOCK_DATABASE } from "../common/MockDatabase";
 import ProductCard from "../layout/productCard/ProductCard";
 import "./UserFavoriteList.scss";
